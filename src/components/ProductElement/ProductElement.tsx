772cbb3b312b097648f54dbe65032f49d145e757
WIN-18FGUV8VB5F\alix <avtimakov70@gmail.com>
2024-07-23 22:56:40 +0300

    Product element's styles change

--- src/components/ProductElement/ProductElement.tsx
+++ src/components/ProductElement/ProductElement.tsx
@@ -18,24 +18,24 @@ export const ProductElement = ({ product }: IProductElement) => {
         src={product.image}
         width={70}
         height={70}
-        className={styles.image}
+        className={cn(styles.image, "rounded-[0.5rem]")}
         alt={product.title}
       />
-      <div className={cn(styles.title, "font-semibold text-[2rem] leading-[2.7rem]")}>{product.title}</div>
-      <div className={cn(styles.price, "text-[2rem] leading-[2.7rem]")}>
+      <div className={cn(styles.title, "self-end font-semibold text-[2rem] leading-[2.7rem]")}>{product.title}</div>
+      <div className={cn(styles.price, "self-end text-[2rem] leading-[2.7rem]")}>
         {numToRub(product.price)}
         {product.oldPrice && <Tag className="ml-4" color="green">{numToRub(product.price - product.oldPrice)}</Tag>}
       </div>
-      <div className={cn(styles.credit, "text-[2rem] leading-[2.7rem]")}>{numToRub(product.credit)}/<span className="ml-2">мес</span> </div>
-      <div className={styles.rate}>
+      <div className={cn(styles.credit, "self-end text-[2rem] leading-[2.7rem]")}>{numToRub(product.credit)}/<span className="text-[1.4rem] leading-[1.9rem] ml-2">мес</span> </div>
+      <div className={cn(styles.rate, "self-end")}>
         <Rating rating={product.reviewAvg as TRating || product.initialRating as TRating} />
       </div>
-      <div className={styles.tags}>{product.tags.map(t => <Tag key={t} color="ghost">{t}</Tag>)}</div>
-      <div className={cn(styles["price-text"], "text-[1.4rem] leading-[1.9rem] text-[var(--black)]")}>цена</div>
-      <div className={cn(styles["credit-text"], "text-[1.4rem] leading-[1.9rem] text-[var(--black)]")}>в кредит</div>
-      <div className={cn(styles["rate-text"], "text-[1.4rem] leading-[1.9rem] text-[var(--black)]")}>{product.reviewCount} {numDecl(product.reviewCount, "отзыв", ["", "а", "ов"])}</div>
+      <div className={styles.tags}>{product.tags.map(t => <Tag key={t} className="mt-3" color="ghost">{t}</Tag>)}</div>
+      <div className={cn(styles["price-text"], "mt-2 text-[1.4rem] leading-[1.9rem] text-[var(--black)]")}>цена</div>
+      <div className={cn(styles["credit-text"], "mt-2 text-[1.4rem] leading-[1.9rem] text-[var(--black)]")}>в кредит</div>
+      <div className={cn(styles["rate-text"], "mt-2 text-[1.4rem] leading-[1.9rem] text-[var(--black)]")}>{product.reviewCount} {numDecl(product.reviewCount, "отзыв", ["", "а", "ов"])}</div>
       <HrLine className={styles.hr} />
-      <div className={cn(styles.description, "mb-6")}>{product.description}</div>
+      <div className={cn(styles.description, "mb-6 leading-[2.4rem] text-justify")}>{product.description}</div>
       <div className={cn(styles.features, "pr-24")}>
         {product.characteristics.map((c, index) => {
           return (
